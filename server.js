3c19e4b12d60fdc856f162a56d80ac03afd12711
Thomas Søvik <thomasovik@gmail.com>
2019-04-26 09:35:55 +0200

    changed to cookie-session

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const express = require('express')
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
+const session = require('cookie-session')
+
+// TODO: uninstall express-session cookie-parser, npm install --save cookie-session
+
 const app = express()
 const logger = require('morgan')
 const routes = require('./routes')
@@ -16,8 +18,7 @@ app.use(express.static(publicFolder))
 
 app.set('view engine', 'ejs')
 app.use(logger('dev'))
-app.use(session({ key: 'user_sid', secret: 'ssshhhhh', saveUninitialized: false, resave: false }))
-app.use(cookieParser())
+app.use(session({ name: 'session', keys: ['username', 'resumeid'] }))
 
 app.use('/', routes)
 
