2a37166813f4f2f78fa51f1ca7d2eb95f7d39a18
Thomas Søvik <thomasovik@gmail.com>
2019-04-26 09:49:29 +0200

    implemented cookie-session

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2/promise')
 const sqlConfig = require('./sqlconfig.js')
 
 function verify (req) {
-  return req.session && req.session.id
+  return req.session && req.session.resumeid
 }
 
 // Redirects to welcome screen if not logged in
@@ -39,11 +39,8 @@ router.route('/login')
       WHERE email = '${req.body.email}' AND password = '${req.body.password}'`)
       const resumeid = result[0][0]['resume']
       if (resumeid) {
-        console.log('session')
-        req.session.resumeid = 1
-        
-        console.log(req.session.resumeid)
-        res.redirect(`/editor/${resumeid}`)
+        req.session.resumeid = resumeid
+        res.redirect(`/editor`)
       } else {
         throw Error('Wrong username or password')
       }
@@ -63,56 +60,50 @@ router.route('/login')
     // }
   })
 
-router.get('/editor', (req, res) => {
-  res.redirect('/editor/1')
-})
+router.get('/editor', sessionChecker, async (req, res) => {
+  let db
+  const resumeid = req.session.resumeid
+  try {
+    db = await mysql.createConnection(sqlConfig)
+    let resume = await db.execute(`
+    SELECT * FROM users
+    INNER JOIN resumes ON users.resume = resumes.id
+    WHERE resumes.id = ${resumeid}`)
+    resume = resume[0][0]
 
-router.route('/editor/:id')
-  .get(sessionChecker, async (req, res, next) => {
-    let db
-    try {
-      db = await mysql.createConnection(sqlConfig)
-      let resume = await db.execute(`
-      SELECT * FROM users
-      INNER JOIN resumes ON users.resume = resumes.id
-      WHERE resumes.id = ${req.params.id}`)
-      resume = resume[0][0]
+    let experience = await db.execute(`
+    SELECT * FROM experiences
+    WHERE resume = ${req.params.id}`)
+    experience = experience[0][0]
 
-      let experience = await db.execute(`
-      SELECT * FROM experiences
-      WHERE resume = ${req.params.id}`)
-      experience = experience[0][0]
+    let education = await db.execute(`
+    SELECT * FROM education
+    WHERE resume = ${resumeid}`)
+    education = education[0][0]
 
-      let education = await db.execute(`
-      SELECT * FROM education
-      WHERE resume = ${req.params.id}`)
-      education = education[0][0]
-
-      if (resume) {
-        res.render('editor', { resume: resume, edu: education, exp: experience, id: req.params.id })
-      } else {
-        throw Error('Resume not found')
-      }
-    } catch (err) {
-      // Not found, redirect to login
-      res.render('welcome', { error: err })
-    } finally {
-      // Close connection
-      if (db) { db.end() }
+    if (resume) {
+      res.render('editor', { resume: resume, edu: education, exp: experience, id: resumeid })
+    } else {
+      throw Error('Resume not found')
     }
-  })
+  } catch (err) {
+    // Not found, redirect to login
+    res.render('welcome', { error: err })
+  } finally {
+    // Close connection
+    if (db) { db.end() }
+  }
+})
 
 router.get('/logout', (req, res) => {
-  console.log(req.cookies)
-  console.log(req.cookies.user_sid)
-  console.log(req.session)
-  res.clearCookie('user_sid')
-  //res.redirect('/')
-  res.end()
+  const name = req.session.username
+  req.session = null
+  res.redirect('/welcome', `Goodybye ${name}`)
 })
 
+// TODO : Anyone can view any resume as long as they are logged in?
 router.route('/resume/:id')
-  .get(async (req, res, next) => {
+  .get(sessionChecker, async (req, res, next) => {
     let db
     try {
       console.log('Connecting to DB')
