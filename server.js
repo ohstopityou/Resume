068b557714ef40da5fedec326bdd0ff04faf6b1b
ohstopityou <thomasovik@gmail.com>
2019-06-05 19:39:39 +0200

    added more comments, improved login css

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 'use strict'
 
 const express = require('express')
+const app = express()
+const routes = require('./routes')
 const session = require('cookie-session')
 
-const app = express()
+// Logger used during development
 const logger = require('morgan')
-const routes = require('./routes')
-
-const PORT = process.env.PORT || 8080
+app.use(logger('dev'))
 
 // Makes files in public folder accessable
 const path = require('path')
 const publicFolder = path.join(__dirname, '/public')
 app.use(express.static(publicFolder))
 
+// Enable template engine ejs
 app.set('view engine', 'ejs')
-app.use(logger('dev'))
 
 // Saves session data to the cookie sent with the clients.
 app.use(session({ name: 'session', secret: 'supersecret' }))
 
+// Use routes from seperate file. --Needs to last--
 app.use('/', routes)
 
+// Set port to cloud default or 8080, then start server.
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => { console.log(`App listening at http://localhost:${PORT}`) })
--- sqlconfig.js
+++ sqlconfig.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// Default SQL connection config
 const config = {
   host     : '127.0.0.1',
   user     : 'root',
@@ -7,6 +8,7 @@ const config = {
   database : 'dk205_AfN19'
 }
 
+// Uses a socketPath when run in production
 if (process.env.NODE_ENV === 'prod') {
   config.socketPath = '/cloudsql/silicon-synapse-232018:europe-north1:dikult205'
 }
